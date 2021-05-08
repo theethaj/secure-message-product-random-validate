629933d053ce256fe14a35dd1ad095eb29784b30
theethaj <bank30827@gmail.com>
2021-05-08 10:44:07 +0700

    Add password validation

--- src/main/java/ku/message/dto/SignupDto.java
+++ src/main/java/ku/message/dto/SignupDto.java
@@ -1,7 +1,6 @@
-
-
 package ku.message.dto;
 
+import ku.message.validation.ValidPassword;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
@@ -17,12 +16,11 @@ public class SignupDto {
     private String username;
 
     @NotBlank
-    @Size(min=12, max=128)
+    @ValidPassword
     private String password;
 
     @NotBlank(message = "First name is required")
-    @Pattern(regexp = "^[a-zA-Z]+$",
-            message = "First name can only contain letters")
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "First name can only contain letters")
     private String firstName;
 
     @NotBlank
@@ -34,4 +32,3 @@ public class SignupDto {
 
     private String role;
 }
-
