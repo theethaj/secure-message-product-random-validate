0f35eb001055594556d19916b49c5934914bd693
theethaj <bank30827@gmail.com>
2021-05-08 10:42:18 +0700

    Add input validation

--- src/main/java/ku/message/dto/SignupDto.java
+++ src/main/java/ku/message/dto/SignupDto.java
@@ -1,13 +1,37 @@
+
+
 package ku.message.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Data
 public class SignupDto {
+
+    @NotBlank
+    @Size(min=4, message = "Username must have at least 4 characters")
     private String username;
+
+    @NotBlank
+    @Size(min=12, max=128)
     private String password;
+
+    @NotBlank(message = "First name is required")
+    @Pattern(regexp = "^[a-zA-Z]+$",
+            message = "First name can only contain letters")
     private String firstName;
+
+    @NotBlank
     private String lastName;
+
+    @Email
+    @NotBlank
+    private String email;
+
     private String role;
 }
 
--- src/main/java/ku/message/model/User.java
+++ src/main/java/ku/message/model/User.java
@@ -23,6 +23,7 @@ public class User {
     private String firstName;
     private String lastName;
     private String role;
+    private String email;
     private Instant createdAt;
 
 }
